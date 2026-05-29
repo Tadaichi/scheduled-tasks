98b25bf55cd3960d124d6eed53aa64275a08965a
Tadaichi <tadaichilopez@gmail.com>
2026-05-28 21:43:13 -0400

    main.py

--- main.py
+++ main.py
@@ -6,33 +6,35 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
-import random
 import smtplib
-import os
+import datetime as dt
+import pandas as pd
+import random
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+my_email = "tadaichilopez@gmail.com"
+my_password = "ctsijmoqdvuyplpn"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+now = dt.datetime.now()
+today_day = now.day
+today_month = now.month
+today = (today_month, today_day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+data = pd.read_csv("birthdays.csv")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    random_num = random.randint(1,3)
+    with open(f"letter_templates/letter_{random_num}.txt") as letter:
+        data = letter.read()
+        data = data.replace("[NAME]", birthday_person["name"])
+        
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()  # email encryption
+        connection.login(user=my_email, password=my_password)
         connection.sendmail(
-            from_addr=MY_EMAIL,
+            from_addr=my_email,
             to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            msg=f"Subject:Happy Birthday!!!\n\n{data}"
         )
